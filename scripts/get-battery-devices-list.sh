468dea5a42dd54f7b68c9d6327bb3419f0df52a0
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-04-20 19:11:06 -0400

    refactor(quick-settings): switch to btctl

--- scripts/get-battery-devices-list.sh
+++ scripts/get-battery-devices-list.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-devices=$("$HOME/.local/bin/get-bt-devices.sh")
+devices=$("$HOME"/.local/bin/btctl.sh get devices)
 
 json_output="["
 
 for name in $devices ; do
-    icon=$("$HOME/.local/bin/get-bt-icon.sh" "$name")
-    charge=$("$HOME/.local/bin/get-bt-charge-level.sh" "$name")
+    icon=$("$HOME"/.local/bin/btctl.sh get charge icon "$name")
+    charge=$("$HOME"/.local/bin/btctl.sh get charge level "$name")
     json_output+="[\"$name\",\"$icon\",$charge],"
 done
 
--- scripts/get-bt-devices-list.sh
+++ scripts/get-bt-devices-list.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-"$HOME/.local/bin/get-bt-devices.sh" |
+"$HOME"/.local/bin/btctl.sh get devices |
     jq -R -s 'split("\n") | map(select(length > 0))'
