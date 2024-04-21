90a95d94a325b1999987dc982c216759e74e6959
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-04-20 22:34:17 -0400

    feat(which-key): add parent key chord info
    + move key chord info to the bottom of the widget
    + move which-key to bottom left corner

--- scripts/which-key.sh
+++ scripts/which-key.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 # options ----------------------------------------------------------------------
-submap="$1"
+new_submap="$1"
+parent_submap="$2"
 
 # variables ====================================================================
-keymap="$(~/.config/hypr/scripts/get-submap-keys.sh "$submap")"
+keymap="$(~/.config/hypr/scripts/get-submap-keys.sh "$new_submap")"
 
 # execution ********************************************************************
 eww update current-keymap="$keymap"
+eww update wm-parent-key-chord="$parent_submap"
 eww open which-key
