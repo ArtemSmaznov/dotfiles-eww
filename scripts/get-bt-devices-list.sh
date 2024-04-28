f969d5b3e67a78510f8b157aebb812261dc23e5e
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-04-28 14:47:09 -0400

    feat(quick-settings): functional bluetooth submenu

--- scripts/get-bt-devices-list.sh
+++ scripts/get-bt-devices-list.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-"$HOME"/.local/bin/btctl.sh get devices connected |
-    jq -R -s 'split("\n") | map(select(length > 0))'
+paired_devices=$("$HOME"/.local/bin/btctl.sh get devices paired)
+
+json_output="["
+
+for name in $paired_devices ; do
+    icon=$("$HOME"/.local/bin/btctl.sh device icon "$name")
+    status=$("$HOME"/.local/bin/btctl.sh device status "$name")
+    json_output+="[\"$name\",\"$icon\",\"$status\"],"
+done
+
+json_output="${json_output%,}]"
+
+echo "$json_output" | jq .
