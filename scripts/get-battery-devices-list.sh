1d9f4d20d8bc336a1c90291c164197613ccac8ee
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-05-07 23:38:54 -0400

    fix(scripts): set fallback charge level 0

--- scripts/get-battery-devices-list.sh
+++ scripts/get-battery-devices-list.sh
@@ -7,6 +7,7 @@ json_output="["
 for name in $connected_devices ; do
     icon=$("$HOME"/.local/bin/btctl.sh get charge icon "$name")
     charge=$("$HOME"/.local/bin/btctl.sh get charge level "$name")
+    [ ! "$charge" ] && charge=0
     state=$("$HOME"/.local/bin/btctl.sh get charge state "$name")
     json_output+="[\"$name\",\"$icon\",$charge,\"$state\"],"
 done
