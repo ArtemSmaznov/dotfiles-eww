c043cf92d3aa735ce1f72f7f90b62c30ede1252d
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-07-10 18:44:47 -0400

    fix(which-key): show current submap in path

--- scripts/which-key.sh
+++ scripts/which-key.sh
@@ -2,11 +2,16 @@
 # options ----------------------------------------------------------------------
 new_submap="$1"
 parent_submap="$2"
+separator=">"
 
 # variables ====================================================================
 keymap="$(~/.config/hypr/scripts/get-submap-keys.sh "$new_submap")"
 
+# setup ________________________________________________________________________
+[ "$parent_submap" ] && submap_path="$parent_submap$separator"
+submap_path+="$new_submap"
+
 # execution ********************************************************************
 eww update current-keymap="$keymap"
-eww update wm-parent-key-chord="$parent_submap"
+eww update wm-parent-key-chord="$submap_path$separator"
 eww open which-key
