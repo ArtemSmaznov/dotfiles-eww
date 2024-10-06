a782a72b989e618aa5b49b746a85eb0b53676c6b
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-10-06 11:04:16 -0400

    chore(monitor): switch side to different port

--- scripts/toggle-status-bar.sh
+++ scripts/toggle-status-bar.sh
@@ -3,13 +3,13 @@
 [ -z "$2" ] && monitor=0 || monitor="$2"
 
 case "$monitor" in
-    0) status_bar="main-bar"   ;;
-    1) status_bar="second-bar" ;;
-    *) status_bar="main-bar"   ;;
+0) status_bar="main-bar" ;;
+1) status_bar="second-bar" ;;
+*) status_bar="main-bar" ;;
 esac
 
 case $action in
-    open)  eww open "$status_bar"          ;;
-    close) eww close "$status_bar"         ;;
-    *)     eww open --toggle "$status_bar" ;;
+open) eww open "$status_bar" ;;
+close) eww close "$status_bar" ;;
+*) eww open --toggle "$status_bar" ;;
 esac
