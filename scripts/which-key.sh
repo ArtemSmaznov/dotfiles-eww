ab131e04c70affeb643aee5f50563518e274733f
Artem Smaznov <ArtemSmaznov@gmail.com>
2025-02-01 11:46:22 -0500

    refactor(which-key): don't remember what this is

--- scripts/which-key.sh
+++ scripts/which-key.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 # options ----------------------------------------------------------------------
+while getopts "p:" opt; do
+    case "$opt" in
+    p) list_of_parents="${OPTARG}" ;;
+    *) exit 1 ;;
+    esac
+done
+shift $((OPTIND - 1))
+
 new_submap="$1"
-submap_parents="${*:2}"
 
 # variables ====================================================================
-keymap="$(~/.config/hypr/scripts/get-submap-keys.sh "$new_submap")"
 
 # functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-function build_json () {
-    json_output="["
-
-    for submap in $submap_parents; do
-        json_output+="\"$submap\","
-    done
-
-    json_output="${json_output%,}]"
-
-    echo "$json_output" | jq .
-}
 
 # setup ________________________________________________________________________
-eww update wm-parent-key-chord="$(build_json)"
-eww update current-keymap="$keymap"
+function process_parents() {
+    echo "$list_of_parents"
+}
+
+wm_keymap_json="$(~/.config/hypr/scripts/get-keys.sh "$new_submap")"
+eww update wm-keybinds-map="$wm_keymap_json"
+
+eww update wm-keychord-parents="$(process_parents)"
 
 # execution ********************************************************************
 eww open which-key
