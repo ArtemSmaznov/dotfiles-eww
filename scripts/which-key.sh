dc39c64a3f75c8901d2a491fcc5707b5ebd1cb0b
Artem Smaznov <ArtemSmaznov@gmail.com>
2025-02-01 13:29:39 -0500

    refactor(which-key): allow script to close the window

--- scripts/which-key.sh
+++ scripts/which-key.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+
+# functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function close() {
+    eww close which-key
+    exit 0
+}
+
 # options ----------------------------------------------------------------------
-while getopts "p:" opt; do
+while getopts "p:c" opt; do
     case "$opt" in
     p) list_of_parents="${OPTARG}" ;;
+    c) close ;;
     *) exit 1 ;;
     esac
 done
@@ -13,16 +21,14 @@ new_submap="$1"
 # variables ====================================================================
 
 # functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-# setup ________________________________________________________________________
 function process_parents() {
     echo "$list_of_parents"
 }
 
+# setup ________________________________________________________________________
 wm_keymap_json="$(~/.config/hypr/scripts/get-keys.sh "$new_submap")"
-eww update wm-keybinds-map="$wm_keymap_json"
-
-eww update wm-keychord-parents="$(process_parents)"
 
 # execution ********************************************************************
+eww update wm-keybinds-map="$wm_keymap_json"
+eww update wm-keychord-parents="$(process_parents)"
 eww open which-key
